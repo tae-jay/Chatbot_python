cb852c61d81e73a2787daf113564529f529c30c6
tae-jay <54900187+tae-jay@users.noreply.github.com>
2019-09-16 19:45:33 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,15 @@
+from chatterbot import ChatBot #import the chatbot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+bot= ChatBot('Bot')
+trainer = ChatterBotCorpusTrainer(bot)
+
+while True:
+    message = input('You:')
+    print(message)
+    if message.strip() == 'Bye':
+        print('ChatBot: Bye')
+        break
+    else:
+        reply = bot.get_response(message)
+        print('ChatBot:', reply)
